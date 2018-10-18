e41940769c143ee3c6f4a1881a605787fa0ac30f
pkaczor <newion@o2.pl>
2018-10-18 14:00:08 +0200

    DefaultJobSchedulingPolicyTest

--- job-scheduling-policy/src/test/scala/pl/newicom/jobman/schedule/DefaultJobSchedulingPolicyTest.scala
+++ job-scheduling-policy/src/test/scala/pl/newicom/jobman/schedule/DefaultJobSchedulingPolicyTest.scala
@@ -1,25 +1,98 @@
 package pl.newicom.jobman.schedule
 import org.scalatest.WordSpec
-import pl.newicom.jobman.schedule.TestJob.testJob
-import pl.newicom.jobman.schedule.event.JobScheduleEntryAdded
+import pl.newicom.jobman.{Job, JobParameters}
+import pl.newicom.jobman.schedule.TestJob.{TestJobType, testJob}
+import pl.newicom.jobman.schedule.event.{EquivalentJobFound, JobScheduleEntryAdded}
 
 class DefaultJobSchedulingPolicyTest extends WordSpec {
 
-  val policyUnderTest = new DefaultJobSchedulingPolicy
+  val policy = new DefaultJobSchedulingPolicy
+  val conf = JobSchedulingConfig(minQueues = 1, maxQueues = 10, queueCapacity = 10)
+  val empty = TestJobSchedule()
 
   "Default job scheduling policy" when {
 
-    "empty schedule" should {
+    "no active queues" should {
 
-      val config = JobSchedulingConfig(1, 1, 10)
-      val schedule = TestJobSchedule()
+      val config = conf.copy(minQueues = 1, maxQueues = 1)
+      val schedule = empty
 
-      val result = policyUnderTest(testJob("j1"), schedule, config)
-
-      "assign new job to queue: 1, pos: 0" in {
-        assert(result == JobScheduleEntryAdded(testJob("j1"), 1, 0))
+      "assign first queue, first position" in {
+        val result = policy(testJob("j"), schedule, config)
+        assert(result == JobScheduleEntryAdded(testJob("j"), 1, 0))
       }
     }
+
+    "single active queue, max queues reached" should {
+
+      val config = conf.copy(minQueues = 1, maxQueues = 1)
+      val schedule = empty.withJob(1, "j1")
+
+      "assign the active queue, last position" in {
+        val result = policy(testJob("j"), schedule, config)
+        assert(result == JobScheduleEntryAdded(testJob("j"), 1, 1))
+      }
+    }
+
+    "min queues not reached" should {
+
+      val config = conf.copy(minQueues = 2)
+      val schedule = empty.withJob(1, "j1")
+
+      "assign new queue, first position" in {
+        val result = policy(testJob("j"), schedule, config)
+        assert(result == JobScheduleEntryAdded(testJob("j"), 2, 0))
+      }
+    }
+
+    "multiple active queues" should {
+
+      val config = conf
+      val schedule = empty
+        .withJob(1, "j1", "j2")
+        .withJob(2, "j3")
+        .withJob(3, "j4", "j5")
+
+      "assign the smallest queue, last position" in {
+        val result = policy(testJob("j"), schedule, config)
+        assert(result == JobScheduleEntryAdded(testJob("j"), 2, 1))
+      }
+    }
+
+    "all queues full" should {
+
+      val config = conf.copy(queueCapacity = 1)
+      val schedule = empty.withJob(1, "j1").withJob(2, "j2")
+
+      "assign new queue, first position" in {
+        val result = policy(testJob("j"), schedule, config)
+        assert(result == JobScheduleEntryAdded(testJob("j"), 3, 0))
+      }
+    }
+
+    "all queues full, max queues reached" should {
+
+      val config = conf.copy(maxQueues = 1, queueCapacity = 1)
+      val schedule = empty.withJob(1, "j1").withJob(2, "j2")
+
+      "assign existing queue, last position" in {
+        val result = policy(testJob("j"), schedule, config)
+        assert(result == JobScheduleEntryAdded(testJob("j"), 1, 1))
+      }
+    }
+
+    "single active queue" should {
+
+      val params = new AnyRef with JobParameters
+      val config = conf
+      val schedule = empty.withJobs(1, Job("j1", TestJobType, params))
+
+      "reject equivalent job" in {
+        val result = policy(Job("j2", TestJobType, params), schedule, config)
+        assert(result == EquivalentJobFound("j2", "j1"))
+      }
+    }
+
   }
 
   implicit def state2schedule(state: JobSchedule.State): JobSchedule =
--- job-scheduling-policy/src/test/scala/pl/newicom/jobman/schedule/TestJobSchedule.scala
+++ job-scheduling-policy/src/test/scala/pl/newicom/jobman/schedule/TestJobSchedule.scala
@@ -1,5 +1,21 @@
 package pl.newicom.jobman.schedule
 import pl.newicom.jobman.Job
 import pl.newicom.jobman.schedule.JobSchedule.Entry
+import pl.newicom.jobman.schedule.TestJob.testJob
+import pl.newicom.jobman.utils._
 
-case class TestJobSchedule(queues: Map[Int, List[Entry]] = Map.empty, waitingList: List[Job] = List.empty) extends JobSchedule.State
+case class TestJobSchedule(queues: Map[Int, List[Entry]] = Map.empty, waitingList: List[Job] = List.empty) extends JobSchedule.State {
+
+  def withJobs(queueId: Int, jobs: Job*): TestJobSchedule =
+    jobs.toList match {
+      case Nil =>
+        this
+      case job :: Nil =>
+        copy(queues.plus(queueId, Entry(job, queueId)))
+      case job :: tail =>
+        withJobs(queueId, job).withJobs(queueId, tail:_*)
+    }
+
+  def withJob(queueId: Int, jobIds: String*): TestJobSchedule =
+    withJobs(queueId, jobIds.map(testJob):_*)
+}
